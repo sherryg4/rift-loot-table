ec26a2efa17ff97405c6c12c4912773a09b44d6c
David Ridner <david.ridner@gmail.com>
2014-11-01 19:13:08 -0400

    Started working on the data model.

---
+++ RiftLootTable/Models/GameVersion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RiftLootTable.Models
+{
+    public class GameVersion
+    {
+        public int GameVersionId { get; set; }
+        public string Name { get; set; }
+        public string GameImagePath { get; set; }
+    }
+}
---
+++ RiftLootTable/Models/RiftLootTableContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace RiftLootTable.Models
+{
+    public class RiftLootTableContext : DbContext
+    {
+        public IDbSet<GameVersion> GameVersions { get; set; }
+    }
+}
